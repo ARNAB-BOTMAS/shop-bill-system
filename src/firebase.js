20dda8b07b30e709fa3c95bd89d7649bf9f17cc9
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2024-02-28 12:58:33 +0530

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhn101QM8GcmzgZelIVQDmfdXngubrJys",
-    authDomain: "resturent-a0482.firebaseapp.com",
-    projectId: "resturent-a0482",
-    storageBucket: "resturent-a0482.appspot.com",
-    messagingSenderId: "975609089799",
-    appId: "1:975609089799:web:4b30e039d60e0035b43fa2"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
